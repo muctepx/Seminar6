a7e58dbc733a8da488042ccd9801103cd3af5a7b
daniil <daniil.chuprin@gmail.com>
2022-10-05 21:40:03 +0300

    addtasks

---
+++ Задача41/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 4
+*/
+
+Console.Clear();
+Console.WriteLine("Введите числа через запятую или пробел: ");
+string[] separatingStrings = { ", ", "  ", ",", " " };
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(separatingStrings, System.StringSplitOptions.RemoveEmptyEntries), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.Write($"Кол-во чисел больше 0: {count}");
